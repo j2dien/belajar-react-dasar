69dbfd0a6fc15cba632b3f3c4adc113730092e04
dien96 <didinrudini12rplb@gmail.com>
2025-08-04 00:25:51 +0700

    feat: implement Product and Online components, add product listing and search functionality in NoteList

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,16 +1,33 @@
-import { useContext } from "react";
+import { useRef, useContext, useState, useMemo } from "react";
 import Note from "./Note";
 import { NotesContext } from "./NoteContext";
 
 export default function NoteList() {
     const notes = useContext(NotesContext);
+    const [search, setSearch] = useState("");
+    const searchInput = useRef(null);
+
+    const filteringNotes = useMemo(() => {
+        console.info("Filtering notes");
+        return notes.filter(note => note.text.includes(search));
+    }, [notes, search])
+
+    function handleSearch() {
+        console.info("Handle Search");
+        setSearch(searchInput.current.value);
+    }
+
     return (
-        <ul>
-            {notes.map(note => (
-                <li key={note.id}>
-                    <Note note={note}/>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <input ref={searchInput} placeholder="Search" />
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+                {filteringNotes.map(note => (
+                    <li key={note.id}>
+                        <Note note={note} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
